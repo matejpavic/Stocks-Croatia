56535c17c5435e7c8401a3290e03dd1984008875
unknown <matej.pavic92@gmail.com>
2021-01-21 21:23:08 +0100

    adding express

--- dionice.js
+++ dionice.js
@@ -1,16 +1,25 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
+const express = require('express');
 
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('This will be a great API')
+})
+
+app.listen(5000, () => console.log('app istening ona a port 5000!!'))
 
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 
 //  Wrong year in the url...host mistake
-// var yyyy = today.getFullYear();
-var yyyy = '2020';
+var yyyy = today.getFullYear();
+// var yyyy = '2020';
 
 today = yyyy + '-' + mm + '-' + dd;
+// let today2 = '2021-01-04';
 
 const url = `https://rest.zse.hr/web/Bvt9fe2peQ7pwpyYqODM/price-list/XZAG/${today}/json`; // site that doesn’t send Access-Control-*
 
@@ -33,6 +42,6 @@ fetch(url)
     // console.log(list);
     // fs.writeFile('podaci.docx', list, () => console.log('saved'));
 })
-.catch((err) => console.log(err))
+.catch((err) => console.log(err + 'VJEROJATNO STE ZAHTJEV POSLALI PRERANO, PODACI SU DOSTUPNI TEK PO ZATVARANJU BURZE ILI NISTE SPOJENI NA INTERNET'))
 
   
